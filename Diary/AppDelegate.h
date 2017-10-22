7a3fc57ad75bd1e78a2c2e8e9cdb9a87471b1514
chenweibin <chenweibin@chenweibindemacbook-pro.local>
2017-10-22 10:39:32 +0800

    Initial Commit

---
+++ Diary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Diary
+//
+//  Created by chenweibin on 2017/10/22.
+//  Copyright © 2017年 chenweibin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Diary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Diary
+//
+//  Created by chenweibin on 2017/10/22.
+//  Copyright © 2017年 chenweibin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
