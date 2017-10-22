1b81010ca54439dea0813f733de37859758aafb6
chenweibin <chenweibin@chenweibindemacbook-pro.local>
2017-10-22 10:53:40 +0800

    第二次

---
+++ Diary/Diary/controller/DiaryVC.h
@@ -0,0 +1,13 @@
+//
+//  DiaryVC.h
+//  Diary
+//
+//  Created by chenweibin on 2017/10/22.
+//  Copyright © 2017年 chenweibin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DiaryVC : UIViewController
+
+@end
---
+++ Diary/Diary/controller/DiaryVC.m
@@ -0,0 +1,31 @@
+//
+//  DiaryVC.m
+//  Diary
+//
+//  Created by chenweibin on 2017/10/22.
+//  Copyright © 2017年 chenweibin. All rights reserved.
+//
+
+#import "DiaryVC.h"
+
+@interface DiaryVC ()
+
+@end
+
+@implementation DiaryVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+     self.view.backgroundColor = [UIColor grayColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
